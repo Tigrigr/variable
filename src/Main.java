992e68d9084fc277d3ed0b793a9ce805c46d46bb
mikha <Mikhailov.dzenphone@gmail.com>
2023-03-20 13:50:00 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-
+        task3();
     }
 
     public static void task1 () {
@@ -28,4 +28,16 @@ public class Main {
         System.out.println(paper);
     }
 
+    public static void task3 () {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;// Пишем код для задачи 3
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
 }
